7efd6b619a222171936dea73346d91045dcea380
Mayank <mayankchaurasia.bsp@gmail.com>
2018-04-08 01:22:14 +0530

    Final push

--- venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/mayank/Desktop/SIH_ExtremeCoders/venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
--- venv/lib/python3.5/fnmatch.py
+++
@@ -1,1 +0,0 @@
-/home/mayank/anaconda3/lib/python3.5/fnmatch.py
